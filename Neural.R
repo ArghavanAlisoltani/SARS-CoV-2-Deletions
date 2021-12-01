1e8673a736dd3e2a0510426f239630bcb07272ab
Arghavan Alisoltani <49700396+ArghavanAlisoltani@users.noreply.github.com>
2021-12-01 11:02:08 -0800

    Delete Neural.R

--- Neural.R
+++
@@ -1,9 +0,0 @@
-# neural network
-install.packages("neuralnet")
-library(neuralnet)
-met<-read.delim(file="2.A.proteinFamily.txt", sep="\t")
-met2<-read.delim(file="2.B.proteinFamily.txt", sep="\t")
-mytable<-data.frame(table(met$X..PRTases_typeI))
-
-mytable2<-data.frame(table(met2$))
-write.csv(mytable2, "virgo_anno_Interpro_table.csv")
